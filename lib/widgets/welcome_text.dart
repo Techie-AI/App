a444744f82d57e0faf3dcc49be18100cbbcaa73f
jainankit0811 <ankitd0811@gmail.com>
2024-07-31 09:18:38 +0530

    create home page

---
+++ lib/widgets/welcome_text.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class WelcomeText extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      'Welcome to your Home Page!',
+      style: TextStyle(fontSize: 24),
+    );
+  }
+}
