acf67fd1e4558df87c9c69233908973f18fdcab4
Jayesh Joshi <divyeshjoshi7474@gmail.com>
2024-08-01 14:21:39 +0530

    changed logos

---
+++ lib/models/loading_indicator.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class LoadingIndicator extends StatefulWidget {
+  const LoadingIndicator({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _LoadingIndicatorState createState() => _LoadingIndicatorState();
+}
+
+class _LoadingIndicatorState extends State<LoadingIndicator> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<int> _animation;
+  List<String> images = [
+    'assets/loading1.png',
+    'assets/loading2.png',
+    'assets/loading3.png',
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    )..repeat();
+
+    _animation = IntTween(begin: 0, end: images.length - 1).animate(_controller);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _animation,
+      builder: (context, child) {
+        return Image.asset(
+          images[_animation.value],
+          height: 100,
+          width: 100,
+        );
+      },
+    );
+  }
+}
