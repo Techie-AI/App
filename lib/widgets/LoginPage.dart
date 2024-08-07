3dab534221f5f0239175b565aa27a2363f2bc29e
jainankit0811 <ankitd0811@gmail.com>
2024-08-07 14:04:16 +0530

    login page update details

--- lib/widgets/LoginPage.dart
+++ lib/widgets/LoginPage.dart
@@ -13,6 +13,8 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _nameController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  bool _isButtonEnabled = false;
 
   @override
   void initState() {
@@ -31,16 +33,16 @@ class _LoginPageState extends State<LoginPage> {
   }
 
   Future<void> _login() async {
-    final String email = _emailController.text;
-    final String name = _nameController.text;
+    if (_formKey.currentState!.validate()) {
+      final String email = _emailController.text;
+      final String name = _nameController.text;
 
-    // Perform validation and authentication here
+      final SharedPreferences prefs = await SharedPreferences.getInstance();
+      await prefs.setString('email', email);
+      await prefs.setString('name', name);
 
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
-    await prefs.setString('email', email);
-    await prefs.setString('name', name);
-
-    _navigateToDashboard(email, name);
+      _navigateToDashboard(email, name);
+    }
   }
 
   void _navigateToDashboard(String email, String name) {
@@ -52,56 +54,105 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 
+  void _validateInputs() {
+    setState(() {
+      _isButtonEnabled =
+          _emailController.text.isNotEmpty && _nameController.text.isNotEmpty;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    final double padding = MediaQuery.of(context).size.width * 0.05;
-    final double cardWidth = MediaQuery.of(context).size.width * 0.8;
-
     return BackgroundWrapper(
       child: Center(
         child: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: padding),
-            child: Container(
-              width: cardWidth,
-              padding: const EdgeInsets.all(16.0),
-              child: Card(
-                elevation: 5,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(16),
-                ),
-                child: Padding(
+          child: LayoutBuilder(
+            builder: (context, constraints) {
+              double padding = constraints.maxWidth * 0.1;
+              double cardWidth = constraints.maxWidth * 0.8;
+
+              if (constraints.maxWidth > 600) {
+                padding = constraints.maxWidth * 0.15;
+                cardWidth = constraints.maxWidth * 0.30;
+              }
+
+              return Padding(
+                padding: EdgeInsets.symmetric(horizontal: padding),
+                child: Container(
+                  width: cardWidth,
                   padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      TextField(
-                        controller: _emailController,
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'Enter your email',
+                  child: Card(
+                    elevation: 5,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Form(
+                        key: _formKey,
+                        onChanged: _validateInputs,
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            // Add Logo and App Name
+                            Image.asset(
+                              'assets/logo.png', // Update with your logo asset path
+                              height: 80,
+                            ),
+                            const SizedBox(height: 16.0),
+                            const Text(
+                              'TechieAi', // Replace with your app name
+                              style: TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            const SizedBox(height: 32.0),
+                            TextFormField(
+                              controller: _emailController,
+                              decoration: const InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: 'Enter your email',
+                              ),
+                              keyboardType: TextInputType.emailAddress,
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Please enter your email';
+                                }
+                                if (!value.endsWith('@gmail.com')) {
+                                  return 'Please enter a Gmail address';
+                                }
+                                return null;
+                              },
+                            ),
+                            const SizedBox(height: 16.0),
+                            TextFormField(
+                              controller: _nameController,
+                              decoration: const InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: 'Enter your name',
+                              ),
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Please enter your name';
+                                }
+                                return null;
+                              },
+                            ),
+                            const SizedBox(height: 32.0),
+                            ElevatedButton(
+                              onPressed: _isButtonEnabled ? _login : null,
+                              child: const Text('Login'),
+                            ),
+                            const SizedBox(height: 16.0),
+                          ],
                         ),
-                        keyboardType: TextInputType.emailAddress,
                       ),
-                      const SizedBox(height: 16.0),
-                      TextField(
-                        controller: _nameController,
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'Enter your name',
-                        ),
-                      ),
-                      const SizedBox(height: 32.0),
-                      ElevatedButton(
-                        onPressed: _login,
-                        child: const Text('Login'),
-                      ),
-                      const SizedBox(height: 16.0),
-                    ],
+                    ),
                   ),
                 ),
-              ),
-            ),
+              );
+            },
           ),
         ),
       ),
