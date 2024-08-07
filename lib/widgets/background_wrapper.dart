9b5b50e504b03cacde60b768c09f80c89706ba0a
Jayesh Joshi <divyeshjoshi7474@gmail.com>
2024-08-07 13:02:18 +0530

    new color

--- lib/widgets/background_wrapper.dart
+++ lib/widgets/background_wrapper.dart
@@ -7,21 +7,6 @@ class BackgroundWrapper extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final List<Color> gradientColors = [
-      const Color(0xFF1a447a),
-      const Color(0xFF1b4177),
-      const Color(0xFF1c3e73),
-      const Color(0xFF1d3b70),
-      const Color(0xFF1e386c),
-      const Color(0xFF1f3466),
-      const Color(0xFF1f3160),
-      const Color(0xFF1f2d5a),
-      const Color(0xFF1e2851),
-      const Color(0xFF1d2348),
-      const Color(0xFF1b1e3f),
-      const Color(0xFF191937),
-    ];
-
     return Scaffold(
       body: Stack(
         fit: StackFit.expand,
@@ -31,12 +16,8 @@ class BackgroundWrapper extends StatelessWidget {
             fit: BoxFit.cover,
           ),
           Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: gradientColors,
-                begin: Alignment.topRight,
-                end: Alignment.bottomLeft,
-              ),
+            decoration: const BoxDecoration(
+              color: Colors.black,
             ),
           ),
           child,
